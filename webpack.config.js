921566f48939d48e45e5807e9a38074ed0f72c06
Giovanni Esposito <g.esposito@instamotion.com>
2018-01-11 19:27:37 +0100

    Add basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+
+  module: {
+    rules: [
+      { test: /.js$/, use: 'babel-loader' }
+    ]
+  }
+};
