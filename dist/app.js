5379323198bfb1d0329aa8fd0abf809322cc7423
Giovanni Esposito <g.esposito@instamotion.com>
2018-01-11 19:28:29 +0100

    Add ignore dist folder

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _test = require('./test.js');
-
-var _test2 = _interopRequireDefault(_test);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-console.log((0, _test2.default)('Giovanni'));
--- dist/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = greetings;
-function greetings(name) {
-  return "Hello World " + name + "!";
-}
