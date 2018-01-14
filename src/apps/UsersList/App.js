d049f056aa72dca9d4e9e0870db0d7c1563ce2cf
Giovanni Esposito <g.esposito@instamotion.com>
2018-01-14 11:34:17 +0100

    Add UserList app

---
+++ src/apps/UsersList/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import UserCardItem from  './components/UserCardItem';
+
+const renderUsers = users => users.map((user, idx) => (
+  <li key={`${user.username}-${idx}`}>
+    <UserCardItem user={user}/>
+  </li>
+));
+
+const UsersList = (props) => {
+  const {
+    emptyMessage,
+    list
+  } = props;
+
+  return list.length
+    ? (<ul>{renderUsers(list)} </ul>)
+    : (
+      <div>
+        <span>{emptyMessage}</span>
+      </div>
+    );
+};
+
+UsersList.propTypes = {
+  list: PropTypes.array.isRequired,
+  emptyMessage: PropTypes.string
+};
+
+export default UsersList;
---
+++ src/apps/UsersList/components/UserCardItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const UserCardItem = props => {
+  return (
+    <div>
+      <img src={props.user.imgUrl} alt="user-image-profile"/>
+      <span>{props.user.username}</span>
+    </div>
+  );
+}
+
+UserCardItem.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string,
+    imgUrl: PropTypes.string
+  }).isRequired
+}
+
+export default UserCardItem;
