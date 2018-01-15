587626232e7dbe4c65c16c140a478a83a101da5b
Giovanni Esposito <g.esposito@instamotion.com>
2018-01-15 08:23:14 +0100

    Add redux devtool options

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import { createStore } from 'redux'
 import App from './App';
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
