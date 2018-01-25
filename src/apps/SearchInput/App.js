1f427dfc3ad9b210d82f588460821e88f913c28d
Giovanni Esposito <g.esposito@instamotion.com>
2018-01-25 20:12:12 +0100

    Add placeholder

--- src/apps/SearchInput/App.js
+++ src/apps/SearchInput/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SearchInput = ({ onInput, value }) => (
+const SearchInput = ({ onInput, value, placeholder }) => (
   <div>
     <input
       type="text"
       value={value}
+      placeholder={placeholder}
       onInput={evt => onInput(evt.target.value)}
     />
   </div>
@@ -14,7 +15,11 @@ const SearchInput = ({ onInput, value }) => (
 SearchInput.propTypes = {
   onInput: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
 };
 
+SearchInput.defaultProps = {
+  placeholder: '',
+};
 
 export default SearchInput;
--- src/apps/SearchInput/App.test.js
+++ src/apps/SearchInput/App.test.js
@@ -8,10 +8,15 @@ import SearchInput from './App';
 configTests();
 
 test('renders an input element', (t) => {
-  const wrapper = shallow(<SearchInput value="mrbabbs" onInput={Function.prototype} />);
+  const wrapper = shallow(<SearchInput
+    value="mrbabbs"
+    onInput={Function.prototype}
+    placeholder="type"
+  />);
 
   t.is(wrapper.find('input').length, 1);
   t.is(wrapper.find('input').prop('value'), 'mrbabbs');
+  t.is(wrapper.find('input').prop('placeholder'), 'type');
 });
 
 test('calls onInput() on typing', (t) => {
