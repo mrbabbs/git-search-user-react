fe7066f15a12a595df8c0fcc64ea89193d50af7c
Giovanni Esposito <g.esposito@instamotion.com>
2018-01-31 19:26:56 +0100

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import SearchPage from '~/pages/Search';
 import './App.less';
 
 const App = () => (
-  <div>
-    <SearchPage />
-  </div>
+  <BrowserRouter>
+    <Route path="/" component={SearchPage} />
+  </BrowserRouter>
 );
 
 export default App;
