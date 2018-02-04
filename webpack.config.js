4ff04e5b6ba96984f1bf2ba541be4a2bf4be70b3
Giovanni Esposito <g.esposito@instamotion.com>
2018-02-04 13:48:49 +0100

    Add separate css from js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WriteWebpackFilePlugin = require('write-file-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const copyFiles = new CopyWebpackPlugin([{
   from: 'src/index.html',
   to: '../index.html'
 }]);
 
+const extractCss = new ExtractTextPlugin({
+  filename: 'app.bundle.css',
+  fallback: 'style-loader'
+});
+
 const getPath = name => path.resolve(__dirname, name);
+const isProd = () => process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: './src/index.js',
@@ -24,21 +31,21 @@ module.exports = {
         use: 'babel-loader',
         exclude: [/node_modules/, /.test.js$/]
       },
-
       {
         test: /.less$/,
-        use: [
-          'style-loader',
+        use: extractCss.extract([
           {
             loader: 'css-loader',
             options: {
               camelCase: 'dashes',
               localIdentName: '[name]__[local]',
-              modules: true
+              modules: true,
+              minimize: isProd(),
+              sourceMap: isProd()
             }
           },
           'less-loader',
-        ]
+        ])
       }
     ]
   },
@@ -49,7 +56,7 @@ module.exports = {
     }
   },
 
-  plugins: [new WriteWebpackFilePlugin(), copyFiles],
+  plugins: [new WriteWebpackFilePlugin(), copyFiles, extractCss],
 
   devServer: {
     contentBase: getPath('dist'),
